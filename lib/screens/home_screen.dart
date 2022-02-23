8ff3ea35354dd4a6bfbbd4571aef228cefba11d8
EDGAR LOPEZ SOSA <16171409@itculiacan.edu.mx>
2022-02-22 22:27:01 -0700

    Agregando rutas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'package:fl_components/screens/screens.dart';
+
 class HomeScreen extends StatelessWidget {
    
   const HomeScreen({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes en Flutter'),
       ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          leading: const Icon(Icons.account_balance_outlined),
+          title: const Text('Nombre de Ruta'),
+          onTap: () {
+
+            // final route = MaterialPageRoute(
+            //   builder: (context) => const ListView2Screen(),
+            //   );
+               
+            // Navigator.push(context, route);
+
+            Navigator.pushNamed(context, '/listview2');
+          },
+        ), 
+        separatorBuilder: (_,__) => const Divider(),
+        itemCount: 10
+      )
     );
   }
 }
